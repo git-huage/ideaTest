dc38bf71a0063163073574dd6feafa9d9c2e2354
qph <651024151@qq.com>
2020-03-23 20:02:34 +0800

    第一次提交：qph

---
+++ src/main/java/com/woniu/test/Test.java
@@ -0,0 +1,24 @@
+package com.woniu.test;
+
+/**
+ * @PACKAGE_NAME: com.woniu.test
+ * @NAME: Test
+ * @USER: huage
+ * @DATE: 2020/3/23
+ * @TIME: 20:00
+ * @YEAR: 2020
+ * @MONTH: 03
+ * @MONTH_NAME_SHORT: 3月
+ * @MONTH_NAME_FULL: 三月
+ * @DAY: 23
+ * @DAY_NAME_SHORT: 周一
+ * @DAY_NAME_FULL: 星期一
+ * @HOUR: 20
+ * @MINUTE: 00
+ * @PROJECT_NAME: ideaTest
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("huage");
+    }
+}
